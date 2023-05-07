f5db291750b518cbbd71b24e0cfa56155a93305e
souvik bhattacharyya <106976472+souvik566@users.noreply.github.com>
2023-05-07 20:21:17 +0530

    Add files via upload

---
+++ 05.js
@@ -0,0 +1,112 @@
+let num1=document.getElementById("num1");
+let num2=document.getElementById("num2");
+function add(){
+    let r=document.getElementById("res");
+    r.value=Number();
+    r.value=(Number(num1.value)+Number(num2.value));
+}
+function sub(){
+    let r=document.getElementById("res");
+    r.value=Number();
+    r.value=(Number(num1.value)-Number(num2.value));
+}
+function mult(){
+    let r=document.getElementById("res");
+    r.value=num1.value*num2.value;
+}
+function div(){
+    let r=document.getElementById("res");
+    r.value=Number();
+    r.value=(Number(num1.value)/Number(num2.value));
+}
+function rem(){
+    let r=document.getElementById("res");
+    r.value=Number();
+    r.value=(Number(num1.value)%Number(num2.value));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
